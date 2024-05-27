443a63bfec8d853979e0a9e4367634f9f4f403df
hwld <0701hw@gmail.com>
2024-05-27 10:04:42 +0900

    projectsを追加

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -1,19 +1,27 @@
 import { Project } from "@/data/projects";
 import Link from "next/link";
 import { ProjectThumbnail } from "./project-thumbnail";
+import { TbCode } from "react-icons/tb";
 
 type Props = { project: Project };
 export const ProjectCard: React.FC<Props> = ({ project }) => {
   return (
     <Link
-      className="w-full md:w-[280px] border border-zinc-700 bg-black/20 rounded-lg shrink-0 p-3 flex flex-col gap-2 overflow-hidden transition-colors group relative"
+      className="w-full md:w-[280px] border border-zinc-700 bg-black/20 rounded-lg shrink-0 p-3 flex flex-col overflow-hidden transition-colors group relative justify-between gap-2"
       href="/"
     >
-      <ProjectThumbnail src={project.imageSrc} interactive />
-
-      <div className="space-y-1 p-2">
-        <div className="text-base font-medium">{project.title}</div>
-        <div>{project.summary}</div>
+      <div className="space-y-2">
+        <ProjectThumbnail src={project.imageSrc} interactive />
+        <div className="space-y-1 p-2">
+          <div className="text-base font-medium">{project.title}</div>
+          <div>{project.summary}</div>
+        </div>
+      </div>
+      <div className="text-xs flex gap-1 text-zinc-400 items-center">
+        <TbCode size={16} />{" "}
+        {`${project.createdAt.getFullYear()}年${
+          project.createdAt.getMonth() + 1
+        }月`}
       </div>
       <div className="absolute inset-0 group-hover:bg-zinc-500/20 transition-colors" />
     </Link>
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -6,6 +6,7 @@ export type Project = {
   tagLabels: ProjectTagLabel[];
   imageSrc: string | undefined;
   summary: string;
+  createdAt: Date;
 };
 
 export type FeaturedProject = Project & {
@@ -17,23 +18,156 @@ export const projects = [
   {
     title: "aluep",
     summary:
-      "Next.jsを使ってフロントエンド、バックエンドもTypeScriptを使って作ったアプリ開発のお題を投稿・検索できるWebアプリ",
+      "Next.jsを使ってフロントエンド、バックエンドもTypeScriptを使って作ったアプリ開発のお題を投稿・検索できるWebアプリ。",
     imageSrc: "aluep.png",
     tagLabels: ["TypeScript", "Next.js", "Prisma", "NextAuth", "Mantine"],
+    createdAt: new Date("2022/11/8"),
   },
   {
     title: "evodo-openapi",
     summary:
-      "フロントとバックエンドが分離されている状態で、APIアクセスを型安全にするためのOpenAPIを試したくて作ったTodoリストアプリ",
+      "フロントとバックエンドが分離されている状態で、APIアクセスを型安全にするためのOpenAPIを試したくて作ったTodoリストアプリ。",
     imageSrc: "evodo-openapi.png",
     tagLabels: ["TypeScript", "React", "hono", "Prisma", "lucia", "shadcn-ui"],
+    createdAt: new Date("2023/12/13"),
   },
   {
     title: "zero-one-ui",
     summary:
-      "Reactを使って、パーツレベルから画面全体のレイアウトまで、さまざまなUIを作るためのプロジェクト",
+      "Reactを使って、パーツレベルから画面全体のレイアウトまで、さまざまなUIを作るためのプロジェクト。",
     imageSrc: "zero-one-ui.png",
     tagLabels: ["TypeScript", "React", "tailwindcss"],
+    createdAt: new Date("2023/11/13"),
+  },
+  {
+    title: "portfolio",
+    summary: "このポートフォリオサイト。",
+    imageSrc: undefined,
+    tagLabels: [],
+    createdAt: new Date("2024/3/5"),
+  },
+  {
+    title: "UI Sandbox",
+    summary:
+      "Dribbleにあるプロジェクトや実際のサービスのUIをトレースしたり、パーツを作るときに使っているFigmaファイル。",
+    imageSrc: "ui-sandbox.png",
+    tagLabels: [],
+    createdAt: new Date("2023/10/4"),
+  },
+  {
+    title: "evodo/axum",
+    summary:
+      "axumをつかったノードベースUIのTodoリスト。サブタスクやタスクのブロックなどを実装している。",
+    imageSrc: "evodo-axum.png",
+    tagLabels: [],
+    createdAt: new Date("2024/1/16"),
+  },
+  {
+    title: "evodo/graphql",
+    summary: "GraphQLを使用したTodoリスト。",
+    imageSrc: "evodo-graphql.png",
+    tagLabels: [],
+    createdAt: new Date("2023/10/14"),
+  },
+  {
+    title: "zero-one-ui/calendar",
+    summary:
+      "カレンダー部分をフルスクラッチで書いたWebカレンダー。ドラッグ&ドロップによるイベント作成や移動など、様々な機能を実装している。",
+    imageSrc: "zou-calendar.png",
+    tagLabels: [],
+    createdAt: new Date("2024/4/21"),
+  },
+  {
+    title: "zeron-one-ui/audio-player",
+    summary:
+      "カスタムaudioを実装したブラウザで動くオーディオプレイヤー。ファイルのドラッグ&ドロップに対応している。",
+    imageSrc: "zou-audio-player.png",
+    tagLabels: [],
+    createdAt: new Date("2024/4/15"),
+  },
+  {
+    title: "zero-one-ui/github-projects",
+    summary:
+      "GitHub Projectsの見た目をできるだけそのままに作ったクローンアプリ。機能もそれなりに実装している。",
+    imageSrc: "zou-github-projects.png",
+    tagLabels: [],
+    createdAt: new Date("2024/3/27"),
+  },
+  {
+    title: "housing-loan-simulator",
+    summary:
+      "住宅ローンの料金を計算するWebアプリ。結果を画像として出力する機能も実装している。",
+    imageSrc: "housing-loan-simulator.png",
+    tagLabels: [],
+    createdAt: new Date("2022/9/20"),
+  },
+  {
+    title: "super-cuma",
+    summary: "RemixとCakePHP4で顧客管理アプリを書き比べたもの。",
+    imageSrc: "super-cuma.png",
+    tagLabels: [],
+    createdAt: new Date("2022/7/28"),
+  },
+  {
+    title: "forester",
+    summary: "Remixを使ったTwitterのクローンアプリ。",
+    imageSrc: "forester.png",
+    tagLabels: [],
+    createdAt: new Date("2022/6/13"),
+  },
+  {
+    title: "qflasher",
+    summary: "素早く反復できることを目指したWebで動く単語帳。",
+    imageSrc: "qflasher.png",
+    tagLabels: [],
+    createdAt: new Date("2021/9/1"),
+  },
+  {
+    title: "mini-games",
+    summary: "Reactで実装したいろんなミニゲーム。",
+    imageSrc: "mini-games.png",
+    tagLabels: [],
+    createdAt: new Date("2022/1/20"),
+  },
+  {
+    title: "50-react-projects",
+    summary:
+      "50ReactProjectsという存在するプロジェクトのうちいくつかを作ってみたもの。",
+    imageSrc: "50-react-projects.png",
+    tagLabels: [],
+    createdAt: new Date("2020/8/15"),
+  },
+  {
+    title: "actix-web-todo",
+    summary:
+      "バックエンドをRustで書いたTodoリスト。バックエンドはシンプルなCRUDだけで、結局ずっとフロントエンドを触っていた。",
+    imageSrc: "actix-web-todo.png",
+    tagLabels: [],
+    createdAt: new Date("2021/5/6"),
+  },
+  {
+    title: "countany",
+    summary:
+      "Next.jsを使用したカウンターを自由に作成できるWebアプリ。一定期間アクセスがないとDBが落ちてログインできなくなる。",
+    imageSrc: "countany.png",
+    tagLabels: [],
+    createdAt: new Date("2020/10/24"),
+  },
+  {
+    title: "react-notes",
+    summary:
+      "gweton-webをReactでリライトしたもの。Reactを初めて使ったWebアプリ。",
+    imageSrc: "react-notes.png",
+    tagLabels: [],
+    createdAt: new Date("2020/2/20"),
+  },
+  {
+    title: "gweton-web",
+    summary:
+      "シンプルなメモ帳で、初めて作ったWebアプリ。この頃はVue.jsを使用していた。",
+    imageSrc: "gweton-web.png",
+    tagLabels: [],
+    createdAt: new Date("2019/10/22"),
   },
 ] as const satisfies Project[];
 
