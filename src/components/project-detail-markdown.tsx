0a94b7100a507faaa61ab97d83830efb7470cc4d
hwld <0701hw@gmail.com>
2024-05-28 11:17:38 +0900

    ProjectDetailMarkdownを実装

---
+++ src/components/project-detail-markdown.tsx
@@ -0,0 +1,93 @@
+import clsx from "clsx";
+import { PropsWithChildren, ComponentPropsWithoutRef } from "react";
+import ReactMarkdown from "react-markdown";
+
+type Props = { children: string };
+export const ProjectDetailMarkdown: React.FC<Props> = ({ children }) => {
+  return (
+    <ReactMarkdown
+      components={{
+        h1: H1,
+        h2: H2,
+        h3: H3,
+        h4: H4,
+        h5: H4,
+        h6: H4,
+        p: P,
+        a: A,
+        img: Img,
+        ul: Ul,
+        ol: Ol,
+        li: Li,
+        strong: Strong,
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
+};
+
+const P = ({ children }: PropsWithChildren) => {
+  return <p className="mt-5 leading-7">{children}</p>;
+};
+
+const headerBaseClass = "flex items-center text-zinc-200 font-bold gap-2 mb-5";
+
+const H1 = ({ children }: PropsWithChildren) => {
+  return (
+    <h1 className={clsx(headerBaseClass, "text-3xl mt-12")}>{children}</h1>
+  );
+};
+
+const H2 = ({ children }: PropsWithChildren) => {
+  return (
+    <h2 className={clsx(headerBaseClass, "text-2xl mt-10")}>{children}</h2>
+  );
+};
+
+const H3 = ({ children }: PropsWithChildren) => {
+  return <h3 className={clsx(headerBaseClass, "text-xl mt-8")}>{children}</h3>;
+};
+
+const H4 = ({ children }: PropsWithChildren) => {
+  return <h4 className={clsx(headerBaseClass, "text-lg mt-6")}>{children}</h4>;
+};
+
+const A = ({ children, href }: ComponentPropsWithoutRef<"a">) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      className="text-sky-400 underline underline-offset-2"
+    >
+      {children}
+    </a>
+  );
+};
+
+const Img = ({ src, alt }: ComponentPropsWithoutRef<"img">) => {
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src={src} alt={alt} className="my-5" />;
+};
+
+const Ul = ({ children }: PropsWithChildren) => {
+  return (
+    <ul className="pl-5 mt-5 list-disc marker:text-zinc-500 group group-has-[ul]:mt-0">
+      {children}
+    </ul>
+  );
+};
+
+const Ol = ({ children }: PropsWithChildren) => {
+  return (
+    <ol className="mt-5 list-decimal marker:text-zinc-400 pl-5">{children}</ol>
+  );
+};
+
+const Li = ({ children }: PropsWithChildren) => {
+  return <li className="my-1">{children}</li>;
+};
+
+const Strong = ({ children }: PropsWithChildren) => {
+  return <strong className="font-semibold text-zinc-200">{children}</strong>;
+};
