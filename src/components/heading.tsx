1502028d85a85c3f4bed6924d6ec7c448c4cc134
hwld <0701hw@gmail.com>
2025-01-18 16:05:28 +0900

    feat: 見出しのデザインを変更

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -3,10 +3,10 @@ import { TbHash } from "@react-icons/all-files/tb/TbHash";
 type Props = { subTitle: string; children: string };
 export const Heading: React.FC<Props> = ({ subTitle, children }) => {
   return (
-    <h2 id={subTitle}>
-      <div className="text-foreground-muted text-xs">{subTitle}</div>
+    <h2 id={subTitle} className="space-y-1">
+      <div className="text-foreground-muted text-sm">{subTitle}</div>
       <a
-        className="flex items-center gap-1 text-lg w-fit"
+        className="flex items-center gap-1 text-lg font-bold w-fit"
         href={`#${subTitle}`}
       >
         <TbHash className="text-lg mt-[1px]" />
