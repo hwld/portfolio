9d3cdfc3eb8df5b668b0db21f6ccf4cd0c71a457
hwld <0701hw@gmail.com>
2025-01-19 00:14:28 +0900

    projects: update

--- src/components/certification-list.tsx
+++ src/components/certification-list.tsx
@@ -7,14 +7,33 @@ export const CertificationList: React.FC = () => {
     <div className="space-y-4">
       <Heading subTitle="certifications">資格</Heading>
       <div className="space-y-2">
-        <Certification>FE / AP</Certification>
-        <Certification>SC / DB / NW / ES</Certification>
+        <Certification href="https://www.ipa.go.jp/shiken/kubun/list.html">
+          基本情報技術者試験
+        </Certification>
+        <Certification href="https://www.ipa.go.jp/shiken/kubun/list.html">
+          応用情報技術者試験
+        </Certification>
+        <Certification href="https://www.ipa.go.jp/shiken/kubun/list.html">
+          情報処理安全確保支援士試験
+        </Certification>
+        <Certification href="https://www.ipa.go.jp/shiken/kubun/list.html">
+          データベーススペシャリスト試験
+        </Certification>
+        <Certification href="https://www.ipa.go.jp/shiken/kubun/list.html">
+          ネットワークスペシャリスト試験
+        </Certification>
+        <Certification href="https://www.ipa.go.jp/shiken/kubun/list.html">
+          エンベデッドシステムスペシャリスト試験
+        </Certification>
       </div>
     </div>
   );
 };
 
-const Certification: React.FC<{ children: ReactNode }> = ({ children }) => {
+const Certification: React.FC<{ children: ReactNode; href: string }> = ({
+  children,
+  href,
+}) => {
   return (
     <div className="ml-2 flex items-center gap-2">
       <TbCircleFilled size={10} /> <div>{children}</div>
