1211b3f527701685479635fd5989f86b1cb2374a
hwld <0701hw@gmail.com>
2025-11-23 14:40:59 +0900

    chore: eslint.config.mjsの更新

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+    "public/**",
+  ]),
   {
     rules: {
-      "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
@@ -22,8 +22,11 @@ const eslintConfig = [
           ignoreRestSiblings: true,
         },
       ],
+      // バグがありそうなので無効化
+      // https://github.com/facebook/react/issues/34775
+      "react-hooks/refs": "off",
     },
   },
-];
+]);
 
 export default eslintConfig;
